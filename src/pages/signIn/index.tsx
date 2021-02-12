1d7ce5ee2688caf178083d4656345aa0e8339e5e
Rafael <rafaelsilvamap@gmail.com>
2021-02-12 11:03:55 -0300

    Creating components input, button and refactoring signin page.

--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
+import Input from '../../components/input';
+import Button from '../../components/button';
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
@@ -11,9 +13,14 @@ const SignIn: React.FC = () => (
       <form>
         <h1>Faça seu login</h1>
 
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
+        <Input
+          icon={FiLock}
+          name="password"
+          type="password"
+          placeholder="Senha"
+        />
+        <Button type="submit">Entrar</Button>
 
         <a href="/forgot">Esqueci minha senha</a>
       </form>
