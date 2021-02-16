be92b333eb760a6083c05fc7d9e3164283444c40
Rafael <rafaelsilvamap@gmail.com>
2021-02-15 23:01:03 -0300

    Creating dashboard header page.

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
+import { FiPower } from 'react-icons/fi';
+import logo from '../../assets/logo.svg';
+import { Container, Header, HeaderContent, Profile } from './styles';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>;
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logo} alt="GoBarber" />
+          <Profile>
+            <img
+              src="https://avatars.githubusercontent.com/u/49591933?s=460&u=3fe631a5a78012f55ebba2f51ac6b0a6fae53526&v=4"
+              alt="Rafael Silva"
+            />
+            <div>
+              <span>Bem-vindo,</span>
+              <strong>Rafael Silva</strong>
+            </div>
+          </Profile>
+
+          <button type="button">
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
+  );
+};
 
 export default Dashboard;
